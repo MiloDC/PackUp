80d0f0721942ada4ce3e2ef856c9fc94f516db30
MiloDC <cooper.milo@outlook.com>
2020-06-20 03:44:10 -0700

    Completed file copying.

--- Core.fs
+++ Core.fs
@@ -9,17 +9,18 @@ open Newtonsoft.Json
 open Newtonsoft.Json.Linq
 
 type DirMap = ResizeArray<Regex * Regex seq>    // directory, files
-type EditMap = string * (Regex * string) seq    // file, (regex, replacement)
+type EditMap = string * (Regex * string) seq    // file_path, (regex, replacement)
 
-type StringStringRszArrMap = Dictionary<string, ResizeArray<string>>
-
-type PackIt =
+type Pack =
     {
-        version     : string
-        files       : (string * DirMap * DirMap) seq    // platform, includes, excludes
-        edits       : (string * EditMap seq) seq        // platform, edits
-        password    : string
-        isLinuxTar  : bool
+        version         : string
+        platforms       : string seq
+        rootDir         : DirectoryInfo
+        outFilePrefix   : string
+        files           : IDictionary<string, DirMap * DirMap>  // platform, (includes, excludes)
+        edits           : IDictionary<string, EditMap seq>      // platform, edits
+        password        : string
+        isLinuxTar      : bool
     }
 
     override this.ToString () =
@@ -27,22 +28,28 @@ type PackIt =
 
         Printf.bprintf sb "version = %s\n" this.version
 
+        Printf.bprintf sb "platforms = %A\n" this.platforms
+
+        Printf.bprintf sb "rootDir = %O\n" this.rootDir
+
+        Printf.bprintf sb "outFile = %s\n" this.outFilePrefix
+
         Printf.bprintf sb "files =\n"
         this.files
-        |> Seq.iter (fun (platform, incl, excl) ->
+        |> Seq.iter (fun (KeyValue (platform, (includes, excludes))) ->
             Printf.bprintf sb "\tplatform \"%s\" =\n" platform
             Printf.bprintf sb "\t\tincludes =\n"
-            incl |> Seq.iter (fun (dir, files) ->
+            includes |> Seq.iter (fun (dir, files) ->
                 Printf.bprintf sb "\t\t\t%O =\n" dir
                 files |> Seq.iter (Printf.bprintf sb "\t\t\t\t%O\n"))
             Printf.bprintf sb "\t\texcludes =\n"
-            excl |> Seq.iter (fun (dir, files) ->
+            excludes |> Seq.iter (fun (dir, files) ->
                 Printf.bprintf sb "\t\t\t%O =\n" dir
                 files |> Seq.iter (Printf.bprintf sb "\t\t\t\t%O\n")))
 
         Printf.bprintf sb "edits =\n"
         this.edits
-        |> Seq.iter (fun (platform, edits) ->
+        |> Seq.iter (fun (KeyValue (platform, edits)) ->
             Printf.bprintf sb "\tplatform \"%s\" =\n" platform
             edits |> Seq.iter (fun (filePath, reRepls) -> 
                 Printf.bprintf sb "\t\t%s =\n" filePath
@@ -56,9 +63,18 @@ type PackIt =
         sb.ToString ()
 
 let [<Literal>] private Version = "1.0.0"
+let private dirSep = Path.DirectorySeparatorChar
+let private reFilePath = Regex "(.+/)([^/]+$)"
+let private reOpts = Regex "^[!]+"
 
-let internal reFilePath = Regex "(.+/)([^/]+$)"
-let internal reOpts = Regex "^[!]+"
+let private normalizePath (path : string) = path.Replace (dirSep, '/')
+
+let regexOf prepString isCaseSensitive (s : string) =
+    Regex (
+        if prepString then
+            (s.Replace(".", "\\.").Replace ("*", ".*") |> sprintf "^%s$").Replace (".*/$", ".*$")
+        else s
+        , if isCaseSensitive then RegexOptions.None else RegexOptions.IgnoreCase)
 
 // BEGIN JSON partial acive patterns
 
@@ -102,21 +118,29 @@ let internal (|JsonStringArray|) (jToken : JToken) =
             else None)
     else Seq.empty
 
-let regexOf prepString isCaseSensitive (s : string) =
-    Regex (
-        if prepString then
-            (s.Replace(".", "\\.").Replace ("*", ".*") |> sprintf "^%s$").Replace (".*/$", ".*$")
-        else s
-        , if isCaseSensitive then RegexOptions.None else RegexOptions.IgnoreCase)
+let readPackItFile (platforms' : Set<string>) caseSensitivity jsonFilePath =
+    let dirsCaseSens, filenamesCaseSens, editsCaseSens =
+        (caseSensitivity &&& 1) > 0, (caseSensitivity &&& 2) > 0, (caseSensitivity &&& 4) > 0
 
-let readPackItFile platform dirsCaseSens filenamesCaseSens editsCaseSens filePath =
+    let jsonFullFilePath =
+        if File.Exists jsonFilePath then
+            (FileInfo jsonFilePath).FullName
+        else jsonFilePath
+
     let json =
         try
-            File.ReadAllText filePath |> JObject.Parse |> Some
+            File.ReadAllText jsonFullFilePath |> JObject.Parse |> Some
         with
         | :? JsonReaderException as exc ->
-            printfn "Error reading JSON from %s" filePath
+            printfn "Error reading JSON from %s" jsonFullFilePath
             None
+        | _ ->
+            printfn "Error reading file %s" jsonFullFilePath
+            None
+
+    let rootDir' =
+        if json.IsSome then (FileInfo jsonFullFilePath).Directory
+        else DirectoryInfo (Directory.GetCurrentDirectory ())
 
     {
         version =
@@ -124,63 +148,91 @@ let readPackItFile platform dirsCaseSens filenamesCaseSens editsCaseSens filePat
             |> Option.bind (fun j -> match j.["version"] with JsonString s -> Some s | _ -> None)
             |> Option.defaultValue Version
 
+        platforms =
+            json
+            |> Option.bind (fun j ->
+                let (JsonStringArray platformArray) = j.["platforms"]
+                platformArray
+                |> Seq.filter (fun p -> platforms'.IsEmpty || platforms'.Contains p)
+                |> Some)
+            |> Option.defaultValue Seq.empty
+
+        rootDir = rootDir'
+
+        outFilePrefix =
+            json
+            |> Option.bind (fun j ->
+                match j.["outfile"] with
+                | JsonString s ->
+                    sprintf "%s%c%s" rootDir'.FullName dirSep s |> Some
+                | _ -> None)
+            |> Option.defaultValue ""
+
         files =
             json
             |> Option.bind (fun j ->
                 match j.["files"] with JsonArrayMap map -> Some map | _ -> None)
             |> Option.bind (fun map ->
                 map
-                |> Seq.map (fun (KeyValue (key, JsonStringArray filePaths)) ->
+                |> Seq.filter (fun (KeyValue (platform, _)) ->
+                    platforms'.IsEmpty || platform.Equals "*" || platforms'.Contains platform)
+                |> Seq.map (fun (KeyValue (platform, JsonStringArray filePaths)) ->
                     let includes, excludes =
                         filePaths
                         |> Seq.choose (fun filePath ->
                             match (reFilePath.Match filePath).Groups with
                             | g when 3 = g.Count -> Some (g.[1].Value, g.[2].Value)
                             | _ -> None)
-                        |> Seq.fold (fun (tmpMap : StringStringRszArrMap) (dir, fileName) ->
-                            match tmpMap.TryGetValue dir with
-                            | true, ra -> ra.Add fileName
+                        |> Seq.fold (fun (dirMap : Dictionary<string, ResizeArray<string>>) (dir, fileName) ->
+                            match dirMap.TryGetValue dir with
+                            | true, fileNamesResizeArray -> fileNamesResizeArray.Add fileName
                             | _ ->
-                                tmpMap.[dir] <- ResizeArray<string> ()
-                                tmpMap.[dir].Add fileName
-                            tmpMap)
-                            (StringStringRszArrMap ())
+                                dirMap.[dir] <- ResizeArray<string> ()
+                                dirMap.[dir].Add fileName
+                            dirMap)
+                            (Dictionary<string, ResizeArray<string>> ())
                         |> Seq.fold (fun (incl : DirMap, excl) (KeyValue (dir, fNames)) ->
                             (if (reOpts.Match dir).Value.Contains '!' then excl else incl)
                                 .Add (
-                                    regexOf true dirsCaseSens (reOpts.Replace (dir, "")),
-                                    fNames |> Seq.map (fun f -> regexOf true filenamesCaseSens f))
+                                    reOpts.Replace (dir, "") |> regexOf true dirsCaseSens,
+                                    fNames |> Seq.map (fun f -> regexOf true filenamesCaseSens f)
+                                )
                             incl, excl)
                             (DirMap (), DirMap ())
 
-                    key, includes, excludes)
+                    platform, (includes, excludes))
+                |> dict
                 |> Some)
-            |> Option.defaultValue Seq.empty
+            |> Option.defaultValue (dict Seq.empty)
 
         edits =
             json
             |> Option.bind (fun j -> match j.["edits"] with JsonMapMap map -> Some map | _ -> None)
             |> Option.bind (fun map ->
                 map
+                |> Seq.filter (fun (KeyValue (platform, _)) ->
+                    platforms'.IsEmpty || platform.Equals "*" || platforms'.Contains platform)
                 |> Seq.choose (fun (KeyValue (platform, edits')) ->
                     match edits' with
-                    | JsonArrayMap map ->
+                    | JsonArrayMap arrayMap ->
                         let editMap =
-                            map
+                            arrayMap
                             |> Seq.choose (fun (KeyValue (filePath, JsonStringArray jArray)) ->
                                 let reRepls =
                                     jArray
                                     |> Seq.choose (fun str ->
-                                        match str.Split (str.[0], StringSplitOptions.RemoveEmptyEntries) with
-                                        | [| reStr; replStr |] -> Some (Regex reStr, replStr)
+                                        match str.Split str.[0] with
+                                        | [| ""; reStr; replStr |] ->
+                                            Some (regexOf false editsCaseSens reStr, replStr)
                                         | _ -> None)
                                 if Seq.length reRepls > 0 then
                                     Some (filePath, reRepls)
                                 else None)
                         Some (platform, editMap)
                     | _ -> None)
+                |> dict
                 |> Some)
-            |> Option.defaultValue Seq.empty
+            |> Option.defaultValue (dict Seq.empty)
 
         password =
             json
@@ -192,3 +244,62 @@ let readPackItFile platform dirsCaseSens filenamesCaseSens editsCaseSens filePat
             |> Option.bind (fun j -> match j.["linux_tar"] with JsonBool b -> Some b | _ -> None)
             |> Option.defaultValue false
     }
+
+let packUp (pack : Pack) =
+//    printfn "%O" pack
+    let workDirPath = sprintf "%s%c_PACKIT_" pack.rootDir.FullName dirSep
+    if Directory.Exists workDirPath then Directory.Delete (workDirPath, true)
+    let workDir = Directory.CreateDirectory workDirPath
+
+    let rootDir = normalizePath pack.rootDir.FullName |> sprintf "%s/"
+    let allFiles =
+        pack.rootDir.GetFiles ("*.*", SearchOption.AllDirectories)
+        |> Seq.map (fun fileInfo ->
+            let dir = (sprintf "%s/" fileInfo.DirectoryName |> normalizePath).Replace (rootDir, "")
+            (if String.IsNullOrEmpty dir then "./" else dir), fileInfo.Name)
+//        |> Seq.take 100
+    let allIncludes, allExcludes =
+        match pack.files.TryGetValue "*" with
+        | true, (incl, excl) -> Seq.toArray incl, Seq.toArray excl
+        | false, _ -> Array.empty, Array.empty
+
+    let printPathLen = 64
+
+    pack.platforms
+    |> Seq.iter (fun platform ->
+        let platformDir = workDir.CreateSubdirectory platform
+        let includes, excludes =
+            match pack.files.TryGetValue platform with
+            | true, (incl, excl) ->
+                incl |> Seq.toArray |> Array.append allIncludes,
+                excl |> Seq.toArray |> Array.append allExcludes
+            | false, _ -> allIncludes, allExcludes
+
+        allFiles
+        |> Seq.filter (fun (dir, fileName) -> 
+            excludes
+            |> Array.exists (fun (reDir, reFileNames) ->
+                reDir.IsMatch dir && reFileNames |> Seq.exists (fun re -> re.IsMatch fileName))
+            |> not)
+        |> Seq.choose (fun (dir, fileName) ->
+            if includes |> Array.exists (fun (reDir, reFileNames) ->
+                reDir.IsMatch dir && reFileNames |> Seq.exists (fun re -> re.IsMatch fileName))
+            then
+                Some (
+                    (sprintf "%s%s%s" rootDir dir fileName) |> Path.GetFullPath,
+                    (sprintf "%s/%s%s" platformDir.FullName dir fileName) |> Path.GetFullPath
+                )
+            else None)
+        |> Seq.iter (fun (srcFile, destFile) ->
+            let printPath = srcFile.Replace (Path.GetFullPath rootDir, "")
+            printPath
+                .Substring(0, if printPath.Length < (printPathLen + 1) then printPath.Length else (printPathLen - 3))
+                .PadRight((if printPath.Length < (printPathLen + 1) then printPath.Length else printPathLen), '.')
+                .PadRight (printPathLen, ' ')
+            |> printf "\r%s"
+
+            let destDir = Path.GetDirectoryName destFile
+            if not <| Directory.Exists destDir then Directory.CreateDirectory destDir |> ignore
+            File.Copy (srcFile, destFile, true)))
+
+//    workDir.Delete true
--- Program.fs
+++ Program.fs
@@ -2,36 +2,35 @@
 
 open System
 
-let private processArgs (args' : string []) =
-    args'
-    |> Array.fold (fun ((result, (jsonFile, platform, caseSens)), opt) arg ->
-        if arg.StartsWith '-' then
-            (result + 1, (jsonFile, platform, caseSens)), arg
-        else
-            match opt with
-            | "" -> (result - 1, (arg, platform, caseSens)), "-"
-            | "-p" ->
-                let a = arg.ToLower ()
-                (result - 1, (jsonFile, (if "all" = a then "*" else a), caseSens)), ""
-            | "-c" -> (result - 1, (jsonFile, platform, snd (Int32.TryParse arg))), ""
-            | _ -> (result + 1, (jsonFile, platform, caseSens)), opt)
-        ((1, ("", "*", 0)), "")
-    |> fst
+let private (|ProcessArgs|) (args : string array) =
+    let result, jsonFile, platforms, caseSens =
+        args
+        |> Array.fold (fun ((res, jf, plats, cs), opt) arg ->
+            if arg.StartsWith '-' then
+                (res + 1, jf, plats, cs), arg
+            else
+                match opt with
+                | "" -> (res - 1, arg, plats, cs), "-"
+                | "-p" -> (res - 1, jf, Set.add (arg.ToLower ()) plats, cs), ""
+                | "-c" -> (res - 1, jf, plats, snd (Int32.TryParse arg)), ""
+                | _ -> (res + 1, jf, plats, cs), opt)
+            ((1, "", Set.empty, 0), "")
+        |> fst
+
+    result, (jsonFile, platforms, caseSens)
 
 [<EntryPoint>]
-let main args =
-    let processArgsResult, (jsonFile, platform, cs) = processArgs args
-    if 0 <> processArgsResult then
-        printfn "Syntax: packit [OPTIONS] JSON_FILE"
-        printfn "\tOptions:"
-        printfn "\t\t-p PLATFORM     Platform to pack (default = all)"
-        printf "\t\t-c #            Bitwise case-sensitivity"
-        printfn " [1 = directories, 2 = filenames, 4 = edits] (default = 0)"
-        1
-    else
-        let p = readPackItFile platform ((cs &&& 1) > 0) ((cs &&& 2) > 0) ((cs &&& 4) > 0) jsonFile
-
-        printfn "%O" p
-        // ...
+let main = function
+    | ProcessArgs (0, (jsonFile, platforms, caseSens)) ->
+        readPackItFile platforms caseSens jsonFile
+        |> packUp
 
         0
+    | _ ->
+        printfn "Syntax: packit [OPTIONS] JSON_FILE"
+        printfn "\tOptions:"
+        printfn "\t\t-p PLATFORM [-p PLATFORM ...] - Pack given platform(s) only"
+        printf "\t\t-c # - Bitwise case-sensitivity"
+        printfn " [1 = directories, 2 = filenames, 4 = edits] (default = 0)"
+
+        1
